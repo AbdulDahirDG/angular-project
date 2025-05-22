36c75af5e30933425e9db41cfe2a292c625887b7
AbdulDahirDG <sldahira@domesticandgeneral.com>
2025-05-22 10:58:36 +0100

    product list item components

--- angular-project/src/app/product-list-olr/product-list-olr.component.ts
+++ angular-project/src/app/product-list-olr/product-list-olr.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input, WritableSignal } from '@angular/core';
+import { ProductListItemComponent } from "../product-list-item/product-list-item.component";
 
 @Component({
   selector: 'app-product-list-olr',
-  imports: [],
+  imports: [ProductListItemComponent],
   templateUrl: './product-list-olr.component.html',
   styleUrl: './product-list-olr.component.scss'
 })
 export class ProductListOLRComponent {
 
   @Input({required: true}) productList!: WritableSignal<{name: string, count: number}[]>
+
+  handleRemove($event: string) {
+    this.productList().filter((product) => product.name !== $event)
+  }
 
 }
